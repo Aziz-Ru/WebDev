9be623d1142c3a1044c962b284fb0754404fda0f
silicon <siliconoxide4@gmail.com>
2023-10-29 21:27:40 +0600

    tailwind basizs add

--- Node/http.js
+++ Node/http.js
@@ -1,20 +1,17 @@
-const http=require('http');
-const server=http.createServer((req,res)=>{
-    if(req.url=='/'){
-        res.write('Hello programmers! ');
-        res.write('How are you doing');
-        res.end();
-    }
-    else if(req.url=='./about'){
-        res.write('This is about page');
-        res.end();
-    }
-    else {
-        res.write('Not Found!');
-        res.end();
-    }
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.write("Hello programmers! ");
+    res.write("How are you doing");
+    res.end();
+  } else if (req.url == "./about") {
+    res.write("This is about page");
+    res.end();
+  } else {
+    res.write(` Not Found!`);
+    res.end();
+  }
 });
 
 server.listen(5500);
-console.log('server is running');
- 
+console.log("server is running");
---
+++ Tailwind_CSS/Configuration/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'my-color':'#202940',
+        'hover-color':'#1ee3dc'
+      }
+    },
+  },
+  plugins: [],
+}
+
